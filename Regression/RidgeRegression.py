ffeeb534407d603c06f57561be305c9f51b2c2b1
Mellon <JackMeiLong@users.noreply.github.com>
2016-08-25 22:37:48 +0800

    Add files via upload

--- Regression/RidgeRegression.py
+++ Regression/RidgeRegression.py
@@ -4,7 +4,8 @@ Created on Wed Feb 24 13:49:21 2016
 
 @author: meil
 """
-from numpy import *
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 class RidgeRegression(object):
@@ -13,62 +14,43 @@ class RidgeRegression(object):
         self.trainset=[]
         self.trainlabel=[]
         self.weight=[]
-        self.k=range(5)
+        self.k=[]
         
     def loaddataset(self,path):
-        fb=open(path,'rb')
-        content=fp.read()
-        fb.close()
-        rowlist=content.splitlines()
-        dataset=[row.split('\t') for row in rowlist if row.strip()]
-        dataset=mat(dataset)
-        return dataset
-    
-    def file2matrix(self,dataset):
-        m,n=shape(dataset)
-        self.trainset=zeors((m,n-1))
-        self.trainlabel=zeros((m,1))
-        
-        self.loaddataset=float(dataset[:,:n-1])
-        self.trainlabel=float(dataset[:,-1])
+        df=pd.read_csv(path)
+        self.trainset=df.values[:,:-1]
+        self.trainlabel=df.values[:,-1]
     
     def calweight(self,k):
-        m,n=shape(self.trainset)
-        xT=mat(self.trainset).T
-        xTx=xT*mat(self.trainset)
-        weight=zeros((n,1))
+        m,n=np.shape(self.trainset)
+        X=self.trainset
+        Y=self.trainlabel        
+        XT=np.transpose(X)
+        XTX=XT.dot(X)
+        weight=np.zeros((n,1))
         
-        if linalg.det(xTx)==0:
-            #ridge regression
-            weight=linalg.inv(xTx+k*eye(n))*xT*mat(self.trainlabel)
+        if np.linalg.det(XTX)==0:
+            weight=np.linalg.inv(XTX+np.eye(n).dot(k)).dot(XT).dot(Y)
         else:
-            #linear regression
-            weight=linalg.inv(xTx)*xT*mat(self.trainlabel)
+            weight=np.linalg.inv(XTX).dot(XT).dot(Y)
 
         self.weight=weight
-        
-        
-    def plottrace(self):
-        #plot ridge trace
-        weight=zeors((shape(self.k)[0]))
-        for k in self.k:
-            weight[k]=self.calweight(k)
-            plt.plot(weight[k],linestyle='-',label='ridge trace & k={0}'.format(k))
-        
-        plt.show()
+        return weight
         
     def predict(self,inX):
-        outY=mat(inX).T*mat(self.weight)
+        outY=np.transpose(inX).dot(self.weight)
         return outY
 
 RR=RidgeRegression()
 path='testSet.txt'
-dataset=RR.loaddataset(path)
+RR.loaddataset(path)
+k=np.linspace(1,10,4)
 
-RR.file2matrix(dataset)
+weight=RR.calweight(k[0])
 
-RR.plottrace()
 
-#inX
-outY=RR.predict(inX)
+#RR.plottrace()
+#
+##inX
+#outY=RR.predict(inX)
 
